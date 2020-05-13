3868915fb98483d83deec30049822e69846d6fd8
dlourennco <davidlimalourenco@gmail.com>
2020-05-13 19:07:29 -0300

    image fix

--- scripts/time.js
+++ scripts/time.js
@@ -10,16 +10,16 @@ function time() {
     let minutes = now.getMinutes()
 
     if (hour > 6 && hour < 12) { //dia
-        document.body.style.background = "url('../images/Morning-brotiN-biswaS.jpg')";
+        document.body.style.background = "url('./images/Morning-brotiN-biswaS.jpg')";
         credits.innerHTML = '<strong> photo by brotiN biswaS from Pexels</strong>';
     } else if (hour >= 12 && hour < 18) { //tarde
-        document.body.style.background = "url('../images/Afternoon-Essow-Kedelina.jpg')";
+        document.body.style.background = "url('./images/Afternoon-Essow-Kedelina.jpg')";
         credits.innerHTML = '<strong>photo by Essow Kedelina from Pexels</strong>';
     } else if (hour >= 18 && hour < 24) { //noite
-        document.body.style.background = "url('../images/Night-Free-Nature-Stock.jpg')";
+        document.body.style.background = "url('./Images/Night-Free-Nature-Stock.jpg')";
         credits.innerHTML = '<strong>photo by Free Nature Stock from Pexels</strong>';
     } else if (hour >= 00 && hour < 6) { //madrugada
-        document.body.style.background = "url('../images/Code-Gally.jpg')";
+        document.body.style.background = "url('./images/Code-Gally.jpg')";
         credits.innerHTML = '<strong>photo by Gally from Pexels</strong>';
     }
 
